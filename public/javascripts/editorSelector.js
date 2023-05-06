448ca1ed21a8d4159144f63fa9078050fd499314
User <user@Users-MacBook-Pro.local>
2023-05-06 22:55:17 +0300

    add javascript functionality

--- public/javascripts/editorSelector.js
+++ public/javascripts/editorSelector.js
@@ -1,25 +1,38 @@
-function editorLoad () {
+function editorLoad() {
     const editor = document.getElementById('editor-parent');
     const editorCSS = document.getElementById('editorCSS-parent');
+    const editorJS = document.getElementById('editorJS-parent');
     editor.style.display = 'block';
     editorCSS.style.display = 'none';
+    editorJS.style.display = 'none';
     // editorCSS.style.display = 'none';
 }
-function editorSelector () {
+function editorSelector() {
     const editorBtn = document.getElementById('editor-btn');
     const editorCSSBtn = document.getElementById('editorCSS-btn');
+    const editorJSBtn = document.getElementById('editorJS-btn');
     const editor = document.getElementById('editor-parent');
     const editorCSS = document.getElementById('editorCSS-parent');
+    const editorJS = document.getElementById('editorJS-parent');
     // show editor and hide editorCSS when editor button is pressed
     editorBtn.addEventListener('click', () => {
-      editor.style.display = 'block';
-      editorCSS.style.display = 'none';
+        editor.style.display = 'block';
+        editorCSS.style.display = 'none';
+        editorJS.style.display = 'none';
+
     });
-  
+
     // show editorCSS and hide editor when editorCSS button is pressed
     editorCSSBtn.addEventListener('click', () => {
-      editor.style.display = 'none';
-      editorCSS.style.display = 'block';
+        editor.style.display = 'none';
+        editorCSS.style.display = 'block';
+        editorJS.style.display = 'none';
+
     });
-  };
+    editorJSBtn.addEventListener('click', () => {
+        editor.style.display = 'none';
+        editorCSS.style.display = 'none';
+        editorJS.style.display = 'block';
+    });
+};
 
